03656616b0a7fb78e953a92240afde15419a89ec
Хайбалиев Руслан Мадиевич <Hajbaliev.RM@OMP.LOCAL>
2024-07-30 17:05:50 +0300

    ”newIcons”

--- index.js
+++ index.js
@@ -9,6 +9,57 @@ const maskOptions = {
 
 const mask = new IMask(numberMask, maskOptions);
 
+function validation(form) {
+  function removeError(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains("error")) {
+      parent.querySelector(".error-label").remove();
+      parent.classList.remove("error");
+    }
+  }
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement("label");
+    errorLabel.classList.add("error-label");
+    errorLabel.textContent = text;
+    parent.classList.add("error");
+    parent.append(errorLabel);
+  }
+  let result = true;
+
+  const allInputs = form.querySelectorAll("input");
+
+  for (const input of allInputs) {
+    removeError(input);
+
+    if (input.dataset.minLength) {
+      if (input.value.length < input.dataset.minLength) {
+        removeError(input);
+
+        createError(input, "Неверный номер");
+        result = false;
+      }
+    }
+
+    if (input.dataset.required == "true") {
+      if (input.value == "") {
+        createError(input, "Поле не заполнено");
+        result = false;
+      }
+    }
+  }
+  return result;
+}
+
+document
+  .getElementById("add-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (validation(this) == true) {
+      alert("Че жмешь");
+    }
+  });
+
 // import { Application } from "@splinetool/runtime";
 
 // const canvas = document.getElementById("canvas3d");
